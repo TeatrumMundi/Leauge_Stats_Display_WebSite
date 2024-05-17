52570ea8ab8ced41ffac0e0ceb2ede9556034d39
TeatrumMundi <michal3659@gmail.com>
2024-05-17 16:05:48 +0200

    Creation of the website

--- index.php
+++ index.php
@@ -2,9 +2,23 @@
 <html lang="pl">
     <head>
         <title>League of Legends - Match History</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="style.css">
     </head>
     <body>
-        <?php echo '<p>Hello World!</p>'; ?>
+        <div class="logo">
+            aaa
+            <div id="login">
+                <p>Login</p>
+            </div>
+        </div>
+        <div id="Menu">
+
+        </div>
+        <div id="Main">
+
+        </div>
     </body>
 
 </html>
