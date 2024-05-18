36f0ac09e9dfedbd780ff934819e87019a513747
TeatrumMundi <michal3659@gmail.com>
2024-05-18 19:14:33 +0200

    Main Page Design + Test of the API key

--- index.php
+++ index.php
@@ -1,26 +1,35 @@
 <!DOCTYPE html>
 <html lang="pl">
-    <head>
-        <title>League of Legends - Match History</title>
+<head>
+        <title>League - Stats</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="style.css">
-    </head>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+</head>
 
-    <body>
-        <header class="top-bar">
-            <div class="logo">
-                <a href=index.php>
-                    <img src="IMG/League-logo.png" alt="LOGO">
-                </a>
-            </div>
-            <div class="login">
-                <button class="login-button">Zaloguj się</button>
-            </div>
-        </header>
+<body>
+
+<div class="container">
+    <form id="search-form">
+        <img src="IMG/logo.png" alt="Logo" class="logo">
+        <div class="search">
+            <span class="search-icon material-symbols-outlined">search</span>
+            <label>
+                <input class="search-input" type="search" placeholder="Search" id="search-input">
+            </label>
+        </div>
+    </form>
+</div>
 
 
+<div class="footer">
+    © Designed by <a href="https://github.com/TeatrumMundi" class="footer-link" target="_blank">TeatrumMundi</a>
+</div>
 
-    </body>
-
+<script src="connect.js"></script>
+</body>
 </html>
